1299eaf05d8b3abaf47bb7b29f7a27e891cc4007
Adam Campbell <adam.ccmpbll@gmail.com>
2023-08-27 21:28:29 -0700

    committing here cuz i dont know how i wanna decouple these components and use client in a page. im thinking go all-in on the page for now, and then create a more modular organism that handles all the useSTtate logic abstracted from page code

--- src/app/components/atoms/Input/Input.js
+++ src/app/components/atoms/Input/Input.js
@@ -2,20 +2,20 @@
 
 /**
  * 
- * @param {string} id 
- * @param {string} type 
- * @param {string} value 
- * @param {string} size 
- * @param {string} labelName
- * @param {function} onChange  
+ * @param {string} id (required) The html identifier string
+ * @param {string} type (optional) The input type to use. Defaults to "text"
+ * @param {string} value (optional) An initial value. Defaults to an empty string "".
+ * @param {string} size (optional) 
+ * @param {string} labelName (optional) The innerText of a label, does not render if undefined.
+ * @param {function} onChange (required) Event handler to mutate input value.
  * @returns 
  */
 export default function Input(props) {
-	const { id, type, value, size, labelName, onChange } = props; // destruct props
+	const { id, type = "text", value = "", size, labelName = undefined, onChange } = props; // destruct props
 	return (
 		<>
-			<label>{labelName}</label>
-			<input id={id} type={type} value={value} size={size} onChange={() => {onChange()}}/>
+			{labelName && <label>{labelName}</label>}
+			<input id={id} type={type} value={value} size={size} onChange={onChange}/>
 		</>
 
 	);
--- src/app/recipes/create/page.js
+++ src/app/recipes/create/page.js
@@ -1,13 +1,25 @@
+'use client'
 
 // reference https://stackoverflow.com/questions/66469913/how-to-add-input-field-dynamically-when-user-click-on-button-in-react-js
+import { useState } from "react";
+import Input from "@/app/components/atoms/Input/Input";
 import InfiniteInput from "@/app/components/molecules/InfiniteInput/InfiniteInput";
 
 export default function CreateRecipe() {
+  const [titleVal, setTitleVal] = useState("");
+  const onTitleChangeHandler = e => {
+    e.preventDefault();
+    setTitleVal(e.target.value);
+  }
 
   return (
-    <div>
-      <InfiniteInput />
-    </div>
-
+    <main>
+      <div>
+        <Input id="recipe-title" value={titleVal} onChange={onTitleChangeHandler} />
+      </div>
+      <div>
+        <InfiniteInput />
+      </div>
+    </main>
   );
 }
